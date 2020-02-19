f7869e99be8263852f639e7bba10f079c32b12e8
Tom Harwood <tharwood26@gmail.com>
2020-02-19 11:48:35 +0000

    updated restaurant categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post '/restaurants', to: 'restaurants#create'
+  resources :restaurants
 end
